a8fab844ed07b14aef57fa55f7449389d0b707f9
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-29 15:01:47 +0100

    set react router

---
+++ src/components/Countries.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Countreis() {
+    return(
+        <div />
+    );
+}
+
+export default Countreis;
---
+++ src/components/RandomCam.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RandomCam() {
+    return(
+        <div />
+    );
+}
+
+export default RandomCam;
